2b5236739703c886635e99d351d30765eca6559f
Zaira Coronado <coronado.zaira@gmail.com>
2021-06-15 22:56:01 -0700

    app.py start

---
+++ app.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
